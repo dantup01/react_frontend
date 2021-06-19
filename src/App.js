561465925fa34a46c5dbf066a4fe6a82fd7b69db
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-06-19 11:18:15 +0100

    npx create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+  super(props);
+    this.state = {
+      tileList:[],
+      activeItem:{
+        id:null,
+        title:'',
+        launch_date:'',
+        status:'',
+      },
+      editing:false,
+    }
+    this.fetchTiles = this.fetchTiles.bind(this)
+  };
+
+  componentWillMount(){
+    this.fetchTiles()
+  }
+
+  fetchTiles(){
+    console.log('Fetching...')
+
+    fetch('http://localhost:8000/api/tiles/')
+    .then(response => response.json())
+    .then(data =>
+      console.log('Data:', data)
+      )
+  }
+
+  render(){
+
+    return(
+      <div className="container">
+        <div id="tile-container">
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+
+                <div style={{flex: 6}}>
+                  <input className="form-control" id="tile-title" placeholder="Add a tile..."/>
+                </div>
+
+                <div style={{flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                </div>
+
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      )
+  }
 }
 
 export default App;
